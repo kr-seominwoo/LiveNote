9d5db0796a3d1e86a8bd97837074adf96028ace6
DESKTOP-GCQ8SDQ\seo <tjalsdnss@gmail.com>
2024-02-27 18:55:09 +0900

    [CREATE] - 프로젝트 생성 스프링, JPA, 오라클, 롬복, 타임리프 gradle, groovy, java 17, boot v3.2.3, jar

---
+++ livenote/src/main/java/com/livenote/LivenoteApplication.java
@@ -0,0 +1,13 @@
+package com.livenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivenoteApplication.class, args);
+	}
+
+}
---
+++ livenote/src/test/java/com/livenote/LivenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivenoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
