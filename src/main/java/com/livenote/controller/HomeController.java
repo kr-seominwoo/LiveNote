03c632e91bd077b49dffd65de619aae8bf16fe35
wl9322 <wl9322@gmail.com>
2024-03-14 20:04:58 +0900

    [UPDATE] #3 - 비디오 HLS 통신 ffmpeg를 사용하여 ts파일을 생성(청크는 최소 8초단위) hls.js를 사용하여 ts파일을 재생

--- src/main/java/com/livenote/controller/HomeController.java
+++ src/main/java/com/livenote/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.livenote.controller;
 
+import com.livenote.Service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+
 @Controller
 public class HomeController {
+    @Autowired
+    VideoService videoService;
+
     @GetMapping("/index")
     public String getIndex(){
         return "index.html";
@@ -16,7 +23,8 @@ public class HomeController {
     }
 
     @GetMapping("/video/{id}")
-    public String getVideo() {
+    public String getVideo() throws IOException {
+        videoService.makeHLS("aaa");
         return "videoDetail.html";
     }
 
