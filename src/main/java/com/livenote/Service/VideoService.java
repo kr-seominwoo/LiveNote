ce59abfb75377908969960e66d7b1c9e3d63ea50
wl9322 <wl9322@gmail.com>
2024-03-27 19:10:02 +0900

    [UPDATE] #3 - 경로수정

--- src/main/java/com/livenote/Service/VideoService.java
+++ src/main/java/com/livenote/Service/VideoService.java
@@ -10,9 +10,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 @Service
 public class VideoService {
+    final String M3U8 = ".m3u8";
+    final String PARSE_TIME = "8";
+
     public int saveVideo(MultipartFile file, String title) {
         Video video = new Video();
 
@@ -20,26 +24,30 @@ public class VideoService {
         return 0;
     }
 
-    public void makeHls(String filePath) throws IOException {
+    public void makeHLS(Long id, String filename) throws IOException {
+        String extension = getExtension(filename);
+        String path = id + "\\" + id;
         FFmpeg ffmpeg = new FFmpeg("src/main/resources/ffmpeg/bin/ffmpeg");
         FFprobe ffprobe = new FFprobe("src/main/resources/ffmpeg/bin/ffprobe");
 
         FFmpegBuilder builder = new FFmpegBuilder()
-                .setInput("src/main/resources/static/1.mp4")
-                .addOutput("src/main/resources/static/1.m3u8")
+                //.overrideOutputFiles(true) // 오버라이드 여부
+                .setInput(path + extension) // 동영상파일
+                .addOutput(path + M3U8) // 설정파일?
                 .addExtraArgs("-profile:v", "baseline") //
                 .addExtraArgs("-level", "3.0") //
                 .addExtraArgs("-start_number", "0") //
-                .addExtraArgs("-hls_time", "8") //
+                .addExtraArgs("-hls_time", PARSE_TIME) //
                 .addExtraArgs("-hls_list_size", "0") //
                 .addExtraArgs("-f", "hls") //
                 .done();
-
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         executor.createJob(builder).run();
     }
 
-
-
-    public
+    public String getExtension(String filename) {
+        StringTokenizer st = new StringTokenizer(filename, ".");
+        st.nextToken();
+        return st.nextToken();
+    }
 }
